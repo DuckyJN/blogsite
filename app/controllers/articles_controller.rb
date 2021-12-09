84797c8c3792608403f7feaeb7300c4af8db7145
Justin Nguyen <jnguye74@gmail.com>
2021-12-09 12:24:28 +0900

    Style article create and show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -60,7 +60,7 @@ class ArticlesController < UsersController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = current_user.articles.find(params[:id])
+      @article = Article.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
